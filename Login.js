185b232724b3f03928dd9ab071169347de5d3498
Gowtham Behara <gothsiddu@gmail.com>
2018-06-30 14:53:30 +0530

    Create basic login form for username & password

--- Login.js
+++ Login.js
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import { FormGroup, ControlLabel, FormControl} from 'react-bootstrap';
 class Login extends React.Component {
 
     render() {
         return (
             <div>
+                <LoginForm />
             </div>
         );
     }
@@ -24,7 +25,30 @@ class LoginForm extends React.Component {
         return this.state.username.length > 4 && this.state.password.length > 4
     }
 
-    
+    handleChange() {
+
+    }
+
+    handleSubmit() {
+
+    }
+
+    render() {
+        return (
+            <div className="loginForm">
+                <form onSubmit={this.handleSubmit}>
+                    <FormGroup controlId="username">
+                        <ControlLabel>Username</ControlLabel>
+                        <FormControl type="text" value={this.state.username} onChange={this.state.handleChange} autoFocus/>                        
+                    </FormGroup>
+                    <FormGroup controlId="password">
+                        <ControlLabel>Password</ControlLabel>
+                        <FormControl type="password" value={this.state.password} onChange={this.state.handleChange} />
+                    </FormGroup>
+                </form>
+            </div>
+        );
+    }
 }
 
 export default Login;
