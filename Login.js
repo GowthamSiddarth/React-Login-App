ac16f3ceca0652023b05c4e126667ebf736f736e
Gowtham Behara <gothsiddu@gmail.com>
2018-07-04 07:35:28 +0530

    Show login success/failure alert

--- Login.js
+++ Login.js
@@ -20,9 +20,11 @@ class LoginForm extends React.Component {
 
         this.state = {
             username: "",
-            password: ""
+            password: "",
+            successAlertShowing: false,
+            failureAlertShowing: false
         };
-        
+
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
@@ -34,7 +36,7 @@ class LoginForm extends React.Component {
     handleChange(e) {
         var elementId = e.target.id;
         var elementValue = e.target.value;
-        this.setState({[elementId]: elementValue});
+        this.setState({ [elementId]: elementValue });
     }
 
     handleSubmit(e) {
@@ -48,52 +50,72 @@ class LoginForm extends React.Component {
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify(this.state)
-        })
-        .then(function(res) {
+        }).then(function (res) {
             return res.json();
-        })
-        .then(function(data) {
-            alert(JSON.stringify(data));
+        }).then(data => {
+            console.log(data);
+            this.setState({ 
+                successAlertShowing: data.found,
+                failureAlertShowing: !data.found
+            });
         });
     }
 
     render() {
         return (
-            <Form horizontal onSubmit={this.handleSubmit}>
-                <FormGroup controlId="username">
-                    <Col componentClass={ControlLabel} sm={4}>
-                        Username
+            <div id="loginFormAndAlert">
+                <Form horizontal onSubmit={this.handleSubmit}>
+                    <FormGroup controlId="username">
+                        <Col componentClass={ControlLabel} sm={4}>
+                            Username
                     </Col>
-                    <Col componentClass={ControlLabel} sm={8}>
-                        <FormControl 
-                            type="text" 
-                            value={this.state.username} 
-                            onChange={this.handleChange}
-                            placeholder="min 5 chars req"
-                            autoFocus />
-                    </Col>                    
-                </FormGroup>
-                <FormGroup controlId="password">
-                    <Col componentClass={ControlLabel} sm={4}>
-                        Password
+                        <Col componentClass={ControlLabel} sm={8}>
+                            <FormControl
+                                type="text"
+                                value={this.state.username}
+                                onChange={this.handleChange}
+                                placeholder="min 5 chars req"
+                                autoFocus />
+                        </Col>
+                    </FormGroup>
+                    <FormGroup controlId="password">
+                        <Col componentClass={ControlLabel} sm={4}>
+                            Password
                     </Col>
-                    <Col componentClass={ControlLabel} sm={8}>
-                        <FormControl 
-                            type="password" 
-                            value={this.state.password} 
-                            placeholder="min 5 chars req"
-                            onChange={this.handleChange} />
-                    </Col>                    
-                </FormGroup>
-                <FormGroup controlId="submit">
-                    <Col smOffset={4} sm={8}>
-                        <button 
-                            type="submit" 
-                            class="btn btn-success"
-                            disabled={!this.validateForm()}>Sign In</button>
-                    </Col>
-                </FormGroup>
-            </Form>
+                        <Col componentClass={ControlLabel} sm={8}>
+                            <FormControl
+                                type="password"
+                                value={this.state.password}
+                                placeholder="min 5 chars req"
+                                onChange={this.handleChange} />
+                        </Col>
+                    </FormGroup>
+                    <FormGroup controlId="submit">
+                        <Col smOffset={4} sm={8}>
+                            <button
+                                type="submit"
+                                class="btn btn-success"
+                                disabled={!this.validateForm()}>Sign In</button>
+                        </Col>
+                    </FormGroup>
+                </Form>
+                <div id="loginSucessAlert">
+                    {
+                        this.state.successAlertShowing ?
+                            <div class="alert alert-success" role="alert">
+                                <strong>Login Success!</strong>.
+                            </div> : null
+                    }
+                </div>
+                <div id="loginFailureAlert">
+                    {
+                        this.state.failureAlertShowing ?
+                            <div class="alert alert-danger" role="alert">
+                                <strong>Login Failure!</strong>.
+                            </div> : null
+                    }
+                </div>
+            </div>
         );
     }
 }
