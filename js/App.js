bfdac30d375e0035e4f5bbf47c95b337d99803ea
Gowtham Behara <gothsiddu@gmail.com>
2018-07-08 15:40:02 +0530

    Create Dashboard component

--- js/App.js
+++ js/App.js
@@ -2,5 +2,7 @@ import React from 'react';
 import ReactDom from 'react-dom';
 
 class App extends React.Component {
-    
+    constructor(props) {
+        super(props);
+    }
 }
---
+++ js/Dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDome from 'react-dom';
+
+class Dashboard extends React.Component {
+    render() {
+        return(
+            <div id="dashboard">
+                Dashboard
+            </div>
+        );
+    }
+}
